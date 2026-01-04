6513ba91bb9ecb5c136b925544500ef17b16e17b
Dinuli <dinuli.nha@gmail.com>
2026-01-04 14:01:54 +0530

    implemented property filtering in SearchPage component

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,12 +1,59 @@
+import { useState } from "react";
+import SearchForm from "../components/SearchForm";
 import properties from "../data/properties.json";
 import PropertyCard from "../components/PropertyCard";
 
 export default function SearchPage() {
+  const [filters, setFilters] = useState({
+    type: "Any",
+    price: [200000, 1000000],
+    bedrooms: "",
+    date: null,
+    postcode: ""
+  });
+
+  const filteredProperties = properties.filter((property) => {
+    if (filters.type !== "Any" && property.type !== filters.type) {
+      return false;
+    }
+
+    if (
+      property.price < filters.price[0] ||
+      property.price > filters.price[1]
+    ) {
+      return false;
+    }
+
+    if (filters.bedrooms !== "Any" && filters.bedrooms !== "") {
+      if (property.bedrooms !== filters.bedrooms) {
+        return false;
+      }
+    }
+
+    if (filters.date) {
+      const added = new Date(property.date);
+      if (added < filters.date) {
+        return false;
+      }
+    }
+
+    if (filters.postcode !== "Any" && filters.postcode !== "") {
+      if (property.postcode !== filters.postcode) {
+        return false;
+      }
+    }
+    return true;
+  });
+
   return (
     <div className="container">
       <h1>Property Search</h1>
+      <SearchForm filters={filters} setFilters={setFilters} />
+
+      <h2>Search Results ({filteredProperties.length})</h2>
+
       <div className="results-grid">
-        {properties.map((property) => (
+        {filteredProperties.map((property) => (
           <PropertyCard 
           key={property.id} 
           property={property} />

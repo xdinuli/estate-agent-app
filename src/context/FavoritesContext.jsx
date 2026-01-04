7d867ceeba4b739b05fce7081de47d2fe15049bd
Dinuli <dinuli.nha@gmail.com>
2026-01-04 17:12:54 +0530

    added drag-and-drop functionality and favorites management to PropertyCard component

--- src/context/FavoritesContext.jsx
+++ src/context/FavoritesContext.jsx
@@ -1,1 +1,35 @@
 import { createContext, useState, useContext } from "react";
+
+const FavoritesContext = createContext();
+
+export function FavoritesProvider({ children }) {
+  const [favorites, setFavorites] = useState([]);
+
+  const addFavorite = (property) => {
+    if (!favorites.find((property) => property.id === property.id)) {
+        setFavorites([...favorites, property]);
+    }
+  };
+
+  const removeFavorite = (id) => {
+    setFavorites(favorites.filter((property) => property.id !== id));
+  };
+
+  const clearFavorites = () => {
+    setFavorites([]);
+  };
+
+  return (
+    <FavoritesContext.Provider value={{ 
+        favorites, 
+        addFavorite, 
+        removeFavorite, 
+        clearFavorites }}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+}
+
+export function useFavorites() {
+  return useContext(FavoritesContext);
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "./App.css";
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./App.css";
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { FavoritesProvider } from './context/FavoritesContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <DndProvider backend={HTML5Backend}>
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
+    </DndProvider>
   </BrowserRouter>,
 );
